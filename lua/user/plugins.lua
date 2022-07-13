add881fb235a120986b43c208118e15e4be1a7d2
arnevm <arne.vanmaele@nexuzhealth.com>
2022-07-13 19:10:43 +0200

    jkl

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,8 +70,7 @@ return packer.startup(function(use)
 	use({ "hrsh7th/cmp-nvim-lua"})
 
 	-- snippets
-	--use({ "L3MON4D3/LuaSnip"}) --snippet engine
-	--use({ "rafamadriz/friendly-snippets"}) -- a bunch of snippets to use
+	use({ "L3MON4D3/LuaSnip"}) --snippet engine
 
 	-- LSP
 	use({ "neovim/nvim-lspconfig"}) -- enable LSP
