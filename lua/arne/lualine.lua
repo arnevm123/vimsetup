ae3c09978ada0845ef594bdf982307945abcc60b
Arne Van Maele <arne.vanmaele@nexuzhealth.com>
2023-05-12 18:23:18 +0200

    jkl

--- lua/arne/lualine.lua
+++ lua/arne/lualine.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	event = "BufEnter",
+	event = "VeryLazy",
 	config = function()
 		local status_ok, lualine = pcall(require, "lualine")
 		if not status_ok then
