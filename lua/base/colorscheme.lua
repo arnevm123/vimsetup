3f61adc21178703a8e86aaa5129b4f5f23aeeba5
arnevm123 <arnevanmaele@gmail.com>
2023-05-20 16:09:08 +0200

    jkl

--- lua/base/colorscheme.lua
+++ lua/base/colorscheme.lua
@@ -4,10 +4,4 @@ return {
 		priority = 1000,
 		dependencies = { "rktjmp/lush.nvim" },
 	},
-	{
-		"rose-pine/neovim",
-		priority = 1000,
-		name = "rose-pine",
-		opts = {disable_background = true,},
-	},
 }
